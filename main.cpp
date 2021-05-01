2f88161bb5fdf1801d4347f6a75900ad6112f500
Marco <marcofuentes1607@gmail.com>
2021-04-30 20:14:12 -0600

    main structure

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+// #include <commons.h>
+
+int main() {
+  return 0;
+}
