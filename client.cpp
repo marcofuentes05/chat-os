1ea44ef4efc585c209c84d6755d9a47ae2554c20
michele benvenuto <ben18232@uvg.edu.gt>
2021-05-19 22:50:29 -0600

    Commands functionality done

--- client.cpp
+++ client.cpp
@@ -27,22 +27,84 @@ void* send_msg_handler(void* arg){
 
   while(1){
     string petitionserializer;
-    cin >> userInput;
+    getline(cin, userInput);
     char firtschar = userInput[0];
     chat::MessageCommunication *message = new chat::MessageCommunication(); 
+    if(firtschar == '/'){
+      string command = userInput.substr(0, userInput.find(" "));
+      cout<< command;
+      if(command == "/help"){
+        printf("Los comandos disponibles son:\n/changestate <estado>: para cambiar a los estados activo, inactivo, ocupado\n/users: para obtener todos los usuarios conectados\n/userinfo <user>: para obtener la informacion de un usuario\n@<nombre de usuario> para enviar un mensaje directo\n/help para imprimir este menu\n");
+      }else if(command == "/changestate"){
+        //fill the set status protocol
+        chat::ChangeStatus *changestatus = new chat::ChangeStatus();
+        changestatus->set_username(name);
+        string data = userInput.substr(userInput.find(" ")+1, userInput.size());
+        changestatus->set_status(data);
+
+        //fill the client petition protocol
+        chat::ClientPetition *petition = new chat::ClientPetition();
+        petition->set_option(3);
+        petition->set_allocated_change(changestatus);
+        petition->SerializeToString(&petitionserializer);
+        strcpy(buffer,petitionserializer.c_str());
+
+        send(sock, buffer, petitionserializer.size() +1 ,0);
+
+      }else if(command == "/users"){
+        //fill the user request protocol
+        chat::UserRequest *inforequest = new chat::UserRequest();
+        inforequest->set_user("everyone");
+
+        //fill the client petition protocol
+        chat::ClientPetition *petition = new chat::ClientPetition();
+        petition->set_option(2);
+        petition->set_allocated_users(inforequest);
+        petition->SerializeToString(&petitionserializer);
+        strcpy(buffer,petitionserializer.c_str());
+
+        send(sock, buffer, petitionserializer.size() +1 ,0);
+
+      }else if(command == "/userinfo"){
+        //fill the user request protocol
+        chat::UserRequest *inforequest = new chat::UserRequest();
+        string wantedUsr = userInput.substr(userInput.find(" ")+1, userInput.size());
+        inforequest->set_user(wantedUsr);
+
+        //fill the client petition protocol
+        chat::ClientPetition *petition = new chat::ClientPetition();
+        petition->set_option(2);
+        petition->set_allocated_users(inforequest);
+        petition->SerializeToString(&petitionserializer);
+        strcpy(buffer,petitionserializer.c_str());
+
+        send(sock, buffer, petitionserializer.size() +1 ,0);
+
+      }else{
+        printf("COMANDO INVALIDO\n");
+      }
+    }
+    
     //Direct Message
-    if(firtschar == '@'){
+    else if(firtschar == '@'){
       chat::ClientPetition *petition = new chat::ClientPetition();
-      string recipient = userInput.substr(1, userInput.find(" "));
-      message->set_message(userInput);
+      //obtain the recipient
+      string recipient = userInput.substr(1, userInput.find(" ")-1);
+      
+      //Fill the message protocol
+      string data = userInput.substr(userInput.find(" ")+1, userInput.size());
+      message->set_message(data);
       message->set_sender(name);
       message->set_recipient(recipient);
+      
+      //Fill the client petition protocol
       petition->set_option(4);
       petition->set_allocated_messagecommunication(message);
       petition->SerializeToString(&petitionserializer);
       strcpy(buffer,petitionserializer.c_str());
       send(sock, buffer, petitionserializer.size() +1 ,0);
     }
+    
     //Broadcast
     else{ 
       chat::ClientPetition *petition = new chat::ClientPetition();
@@ -74,6 +136,7 @@ void* recieve_msg_handler(void* arg){
   }
 }
 
+//main
 int main(int argc, char *argv[]) {
   if (argc <= 3) {
     printf("Lacking info\n");
@@ -84,14 +147,10 @@ int main(int argc, char *argv[]) {
   name = userName;
   char* serverIP = argv[2];
   int serverPort = atoi(argv[3]);
-  // char message[50] = "Hello from client, my name is: ";
-  // printf("CONNECTED TO: %s\nON PORT: %d\nWELCOME %s\n",serverIP, serverPort, userName);
+  string petitionserializer;
+  char regbuffer[LENGTH] = {};
 
   struct sockaddr_in serv_addr;
-  // char *hello = "Hello from client, my name is: ";
-  // strcat(message, hello);
-  //strcat(message, userName);
-  //char buffer[1024] = {0};
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     printf("\n Socket creation error \n");
     return -1;
@@ -110,12 +169,21 @@ int main(int argc, char *argv[]) {
     printf("\nConnection Failed \n");
     return -1;
   }
-  
-  // send(sock, message, strlen(message), 0);
-  // printf("Hello message sent\n");
-  // valread = read(sock, buffer, 1024);
-  // printf("%s\n", buffer);
-  // return 0;
+
+  //send the register petition
+  chat::ClientPetition *petition = new chat::ClientPetition();
+  chat::UserRegistration *reg = new chat::UserRegistration();
+  reg->set_username(name);
+  reg->set_ip(serverIP);
+
+  petition->set_option(1);
+  petition->set_allocated_registration(reg);
+
+  petition->SerializeToString(&petitionserializer);
+  strcpy(regbuffer,petitionserializer.c_str());
+  send(sock, regbuffer, petitionserializer.size() +1 ,0);
+
+
   pthread_t send_msg_thread;
   int send_msg_thread_success; 
   send_msg_thread_success = pthread_create(&send_msg_thread, NULL, send_msg_handler, NULL);
