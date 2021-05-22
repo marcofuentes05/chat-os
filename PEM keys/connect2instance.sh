6cf29b66c7c955a59c372689adc78b4d40480a7c
Marco <marcofuentes1607@gmail.com>
2021-05-21 18:01:20 -0600

    confirmation

--- PEM keys/connect2instance.sh
+++ PEM keys/connect2instance.sh
@@ -1,1 +1,1 @@
-ssh -o ServerAliveInterval=180 -o ServerAliveCountMax=2 -i instance-medium-chat.pem ubuntu@3.15.239.221
+ssh -o ServerAliveInterval=180 -o ServerAliveCountMax=2 -i “instance-medium-chat.pem” ubuntu@ec2-3-15-239-190.us-east-2.compute.amazonaws.com
